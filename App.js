0975becdf4af67db7dd6458d1ac4ecf567bc1b53
Xybra <110640726+xyberkid@users.noreply.github.com>
2024-03-05 14:35:21 +0000

    Updated App.js, app.json, and package.json

--- App.js
+++ App.js
@@ -1,20 +1,52 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+
+import LoginScreen from "./screens/LoginScreen";
+import SignUpScreen from "./screens/SignUpScreen";
+import ForgetPasswordScreen from "./screens/ForgetPasswordScreen";
+import MainScreen from "./screens/MainScreen";
+import AddProductScreen from "./screens/AddProductScreen";
+import ProductDetailScreen from "./screens/ProductDetailScreen";
+import UserProfileToggle from "./screens/UserProfileToggle";
+import EditUserScreen from "./screens/EditUserScreen";
+import EditProductScreen from "./screens/EditProductScreen";
+import PaymentScreen from "./screens/PaymentScreen";
+
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Login">
+        <Stack.Screen
+          name="Login"
+          options={{ headerShown: false }}
+          component={LoginScreen}
+        />
+        <Stack.Screen name="SignUp" component={SignUpScreen} />
+        <Stack.Screen name="Forget Password" component={ForgetPasswordScreen} />
+        <Stack.Screen
+          name="MainScreen"
+          options={{ headerShown: false }}
+          component={MainScreen}
+        />
+        <Stack.Screen name="Sell" component={AddProductScreen} />
+        <Stack.Screen
+          name="Product Detail"
+          // options={{ headerShown: false }}
+          component={ProductDetailScreen}
+        />
+        <Stack.Screen
+          name="ProductDetails"
+          options={{ headerShown: false }}
+          component={ProductDetailScreen}
+        />
+        <Stack.Screen name="UserProfileToggle" component={UserProfileToggle} />
+        <Stack.Screen name="Edit User Profile" component={EditUserScreen} />
+        <Stack.Screen name="Edit Product" component={EditProductScreen} />
+        <Stack.Screen name="Payment" component={PaymentScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
